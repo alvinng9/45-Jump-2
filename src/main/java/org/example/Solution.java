960d5621c4154cfebf715564f7fbad4763b32a49
Alvin <alvinngg9@gmail.com>
2024-08-29 17:28:36 -0700

    Update Solution.java

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -4,12 +4,18 @@ import java.util.Arrays;
 
 public class Solution {
     public int jump(int[] nums) {
+        //create an int array with the same length as the given int array
         int[] result = new int[nums.length];
-        Arrays.fill(result, 999999);
+        //fill all the indexes of the newly created with '99999'
+        Arrays.fill(result, 99999);
+        //create the base case
         result[0] = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (result[i] != 999999) {
-                for (int j = 1; j <= nums[i] && j + i < nums.length; j++) {
+        //iterate through each jump
+        for (int i = 0; i < result.length; i++) {
+            //check if it's possible to jump to the current spot
+            if (result[0] != 99999) {
+                //find the least amount of jump needed to get to next few spots
+                for (int j = 1; j <= nums[i] && j + i < result.length; j++) {
                     result[i + j] = Math.min(result[i + j], result[i] + 1);
                 }
             }
